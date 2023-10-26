f12468cb9af6f797d5e2a5e2baf846a668e19a22
Sebastián Vallejo <=>
2023-10-25 21:55:08 -0300

    insertar archivo ahora lo hace de manera ordenada

--- listArchivos.c
+++ listArchivos.c
@@ -50,14 +50,16 @@ list_archivos Snoc(archivo a, list_archivos ar){
 	list_archivos auxAr = new(nodo_listArchivos);
 	auxAr->archs = a;
 	auxAr->siguiente = NULL;
-	if(ar == NULL)
+	list_archivos iter = ar;
+
+	while(iter != NULL && iter->siguiente != NULL){
+		iter = iter->siguiente;
+	}
+	if(iter == NULL)
 		return auxAr;
 	else{
-		list_archivos inicio = ar;
-		while(inicio->siguiente != NULL)
-			inicio = inicio->siguiente;
-		inicio->siguiente = auxAr;
-		return inicio;
+		iter->siguiente  = auxAr;
+		return ar;
 	}
 }
 
@@ -65,101 +67,50 @@ list_archivos Snoc(archivo a, list_archivos ar){
 list_archivos Insert_listArchivos(archivo a, list_archivos ar){
 //Inserta un archivo de manera ordenada, en una lista ordenada
 
-	list_archivos listaAux = ar, listaRet;
+	list_archivos listaAux = ar;
+	list_archivos listaRet = Null_listArchivos();
 	archivo archAux;
 	Cadena nombreArch, nombreInsert;
-	bool salir = false;
-	cout << "Estoy dentro de la funcion" << endl;
-
-	//Bloque if de testeo
-	if(listaAux == NULL){
-		cout << "listaAux es nula" << endl;
-		listaRet=Cons_listArchivos(a, listaRet);
-		//salir = true;
-	}else
-		cout << "La lista no es nula" << endl;
-
-		if(listaRet == NULL){
-		cout << "listaRet es nula" << endl;
-	}else
-		cout << "La lista no es nula" << endl;
+	nombreInsert = Nombre_archivo(a);
+	bool agregado = false;
 	
-	while(listaAux != NULL && !salir){
-		cout << "While" << endl;
+	if(listaAux == NULL){
+		listaRet = Cons_listArchivos(a, listaRet);
+		
+	} else if(Tail_listArchivos(listaAux) == NULL){
 		archAux = Head_listArchivos(listaAux);
 		nombreArch = Nombre_archivo(archAux);
-
-		nombreInsert = Nombre_archivo(a);
-		int resulrComp = strcmp(nombreArch,nombreInsert);
-
-		if(resulrComp < 0 ){
-			cout << "Resultado de la comparacion: "<< resulrComp << endl;
-			if(listaRet == NULL){
-				cout << "la lista es vacia" << endl;
-				listaRet->archs = a;
-				listaRet->siguiente = listaAux;
-				salir = true;
-			}else{
-				cout << "La lista no es vacia" << endl;
-				listaRet = Cons_listArchivos(a,listaRet);
-				listaRet->siguiente = listaAux;
-				salir = true;
-			}
-		}else{
-			cout << "Resultado de la comparacion(No ordenada): "<< resulrComp << endl;
+		int resultComp = strcmp(nombreArch,nombreInsert);
+		if(resultComp >0){
+			listaRet = Cons_listArchivos(a, listaRet);
 			listaRet = Snoc(archAux,listaRet);
+		} else {
+			listaRet = Snoc(archAux, listaRet);
+			listaRet = Snoc(a, listaRet);
 		}
-		listaAux = listaAux->siguiente;
-	}
-	cout << "Voy a retornar"<< endl;
-	return listaRet;
-
-
-
-
-	/*
-
-
-
-
-	list_archivos aux = ar,listAux;
-	archivo auxA;
-
-	while (ar != NULL){
-		auxA = Head_listArchivos(aux);
-		Cadena nombArch = Nombre_archivo(auxA);
-
-		Cadena nombInsert = Nombre_archivo(a);
-
-		if(strcmp(nombArch,nombInsert) > 0 ){
-
-			while(ar != NULL){
-
-				if(strcmp(nombArch,nombInsert) > 0){ 
-					if(listAux == NULL){
-						auxA = Head_listArchivos(ar);
-						listAux->archs = auxA;
-						listAux->siguiente = ar;
-					}else {
-					auxA = Head_listArchivos(ar);
-					}
-				}
-
-
-
-				ar = ar->siguiente;
+		
+	} else {
+		while(listaAux!=NULL){
+			archAux = Head_listArchivos(listaAux);
+			nombreArch = Nombre_archivo(archAux);
+			int resultComp = strcmp(nombreArch,nombreInsert);
+			
+			if(resultComp > 0){
+				if(!agregado){
+					listaRet = Snoc(a, listaRet);
+					agregado = true;
+				} 
+				listaRet = Snoc(archAux, listaRet);
+			} else {
+				listaRet = Snoc(archAux, listaRet);
+				if(Tail_listArchivos(listaAux) == NULL)
+					listaRet = Snoc(a,listaRet);
 			}
-
-			cout << "esta ordenad "<<endl;
-		}else{
-			archivo archLista = Head_listArchivos(aux);
-			listAux->archs = archLista;
-			cout << "esta desordenad "<<endl;
+			
+			listaAux = listaAux->siguiente;
 		}
-
-		aux = Tail_listArchivos(aux);
 	}
-	return ar;*/
+	return listaRet;
 }
 
  bool IsEmpty_listArchivos(list_archivos ar){
